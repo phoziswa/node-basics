d1b0a55e7ea89df4772e27e05ce1d0d46e862563
phoziswa <phoziswanethe14@gmail.com>
2019-09-09 15:27:48 +0200

    tested the figlet

--- greet-xola.js
+++ greet-xola.js
@@ -6,3 +6,14 @@ const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
+
+var figlet = require('figlet');
+ 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
